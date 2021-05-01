76026eea2ad242ee592f71b20ead54b806b205d1
pednucci <pedropetenucci@gmail.com>
2021-05-01 19:15:39 -0300

    server: database connection

---
+++ src/database/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+dotenv.config();
+
+async function connection(){
+    const connect = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS
+    })
+    return connect;
+}
+
+module.exports = connection;
