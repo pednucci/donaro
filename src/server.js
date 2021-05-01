7842a7acd25e441b608f89bf2a1e808c432dd795
pednucci <pedropetenucci@gmail.com>
2021-05-01 19:14:53 -0300

    server: packages added and configured

--- src/server.js
+++ src/server.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const hbs = require('express-handlebars');
+const router = require('./routes');
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.engine('hbs', hbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+app.use(router)
 
 app.listen(PORT, () => {
     console.log(`Server is running in port ${PORT}`)
