937dc09f33c9983cfd98f0ea230452078eed424c
pednucci <pedropetenucci@gmail.com>
2021-05-01 19:17:18 -0300

    server: user migration added

---
+++ src/database/migrations/20210501214250_usuario.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+exports.up = function(knex) {
+  return knex.schema.createTable('usuario', (table) => {
+    table.increments('cd_usuario').primary().notNullable();
+    table.string('nm_usuario', 45).notNullable();
+    table.string('cd_cpf_usuario', 11).notNullable();
+    table.string('cd_celular_usuario', 11).notNullable();
+    table.string('cd_estado_usuario', 2).notNullable();
+    table.string('nm_cidade_usuario', 255).notNullable();
+    table.string('cd_senha_usuario', 45).notNullable();
+    table.string('cd_email_usuario', 75).notNullable();
+    table.boolean('cd_isAdmin_usuario').defaultTo(false).notNullable();
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('usuario');
+};
