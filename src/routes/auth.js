de3f99c1fd0496f4e9db1b2e5a8fe44488f3f337
pednucci <pedropetenucci@gmail.com>
2021-05-02 18:20:09 -0300

    server: auth route added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const router = express.Router();
 const db = require('../database/database');
+const registerController = require('../controllers/registerController');
 
 router.get('/cadastro', (req, res) => {
     res.render('login/cadastro')
 })
 
-router.post('/cadastro', async (req, res) => {
-
-})
+router.post('/cadastro', registerController.register)
 
 module.exports = router;
 
