df9e74e0d9e0da17facc2113c37f664a5046e1e5
pednucci <pedropetenucci@gmail.com>
2021-05-01 23:42:13 -0300

    server: city data changed

--- src/database/migrations/20210501214250_usuario.js
+++ src/database/migrations/20210501214250_usuario.js
@@ -8,7 +8,7 @@ exports.up = function(knex) {
     table.string('cd_cpf_usuario', 11).notNullable();
     table.string('cd_celular_usuario', 11).notNullable();
     table.string('sg_estado_usuario', 2).notNullable();
-    table.string('nm_cidade_usuario', 255).notNullable();
+    table.string('nm_cidade_usuario', 32).notNullable();
     table.string('cd_senha_usuario', 45).notNullable();
     table.string('cd_email_usuario', 75).notNullable();
     table.boolean('cd_isAdmin_usuario').defaultTo(false).notNullable();
