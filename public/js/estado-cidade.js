3fcf8760da54567699004d11416cc7059f02f62c
Yago Mouro <yagormouro@gmail.com>
2021-05-03 15:00:20 -0300

    web: cadastrar-pedido state/city api added

--- public/js/estado-cidade.js
+++ public/js/estado-cidade.js
@@ -33,7 +33,7 @@ function getCities(event) {
     .then(res => res.json())
     .then(cities => {
       for (const city of cities) {
-        citySelect.innerHTML += `<option value='0'"${city.nome}">${city.nome}</option>`
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
       }
 
       citySelect.disabled = false
