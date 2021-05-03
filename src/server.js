c6ed1e936c183cee850c28e2b41b20f6e6bcae4f
pednucci <pedropetenucci@gmail.com>
2021-05-02 22:34:15 -0300

    server: middlewares, session and configured passport strategy

--- src/server.js
+++ src/server.js
@@ -8,17 +8,25 @@ const hbs = require('express-handlebars');
 const router = require('./routes');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+require('./config/auth')(passport);
 
 app.use(session({
-    secret: "19ejasdjn21sjia1",
-    resave: true,
-    saveUninitialized: true
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
 }))
-app.use(flash())
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 app.use((req, res, next) => {
     res.locals.successMsg = req.flash("successMsg");
     res.locals.errorMsg = req.flash("errorMsg");
+    res.locals.error = req.flash("error");
+    res.locals.user = req.user || null;
     next()
 })
 
