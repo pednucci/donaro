e432d63882d41894ae561ff95b2c3fedaaf5d675
pednucci <pedropetenucci@gmail.com>
2021-05-05 11:43:08 -0300

    server: trash removed

--- src/controllers/newOrderController.js
+++ src/controllers/newOrderController.js
@@ -13,8 +13,6 @@ exports.newOrder = async (req, res) => {
     let fileUpload;
     let uploadPath;
 
-    //console.log(req.body.tpf[0])
-
     try {
         const idUser = req.user[0].cd_usuario;
         const pedido = await conn.query(`INSERT INTO pedido(cd_usuario_pedido, nm_titulo_pedido,
@@ -71,55 +69,6 @@ exports.newOrder = async (req, res) => {
     catch(err) {
         console.log(err)
     }
-
-    /*
-    const titulo = req.body[0].titulo
-    const dataPed = new Date(req.body[0].data);
-    const meta = req.body[0].meta;
-    const estado = req.body[0].estado;
-    const cidade = req.body[0].cidade;
-    const descricao = req.body[0].descricao;
-    //let fileUpload;
-    //let uploadPath;
-
-    try {
-        
-        const idUser = req.user[0].cd_usuario;
-        const pedido = await conn.query(`INSERT INTO pedido(cd_usuario_pedido, nm_titulo_pedido,
-            nm_meta_pedido, dt_encerramento_pedido, nm_cidade_pedido,
-            sg_estado_pedido, ds_acao_pedido) VALUES(?,?,?,?,?,?,?)
-            `, [idUser, titulo, meta, dataPed, cidade, estado, descricao]);
-
-        const idPedido = pedido[0].insertId;
-
-        
-        fileUpload.mv(uploadPath, async () => {
-            await conn.query(`UPDATE pedido SET cd_imagem_pedido = ? WHERE
-            cd_pedido = ?`, [fileUpload.name, idPedido])
-        })
-
-        req.body[1].forEach(async alimento => {
-            await conn.query(`INSERT INTO alimento
-                (nm_alimento, cd_pedido, nm_medida_alimento, qt_alimento, nm_tipoFisico_alimento)
-                VALUES(?,?,?,?,?)`
-                , [alimento.alimento, idPedido, alimento.medida, alimento.quantidade, alimento.tipoFisic])
-        });
-
-        if(meta == 'fechada'){
-            await conn.query(`
-            UPDATE pedido SET qt_total_pedido = 
-            (select sum(qt_alimento) from alimento where cd_pedido = ?) WHERE cd_pedido = ?
-            `, [idPedido, idPedido])
-        }
-
-
-        res.status(200).send("Pedido cadastrado com sucesso!")
-    }
-    catch(err) {
-        console.log(err)
-        res.status(500).send("Erro ao cadastrar o pedido!")
-    }
-    */
     
     await conn.end();
 }
