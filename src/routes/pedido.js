af359c8a4e7bd3375b4275c06273f7f2316af4db
pednucci <pedropetenucci@gmail.com>
2021-05-05 11:30:45 -0300

    server: old routes removed

--- src/routes/pedido.js
+++ src/routes/pedido.js
@@ -9,16 +9,6 @@ router.get('/criar', isAuth, (req, res) => {
 
 router.post('/cadpedido', newOrderController.newOrder);
 
-router.get('/cadaccept', (req, res) => {
-    req.flash('successMsg', 'Pedido cadastrado com sucesso!')
-    res.redirect('/');
-})
-
-router.get('/cadrecused', (req, res) => {
-    req.flash('errorMsg', 'Ocorreu um erro ao cadastrar seu pedido!')
-    res.redirect('/');
-})
-
 module.exports = router
 
 
