262325f5a3383ed85beae2513917db51b6c2bc44
pednucci <pedropetenucci@gmail.com>
2021-05-05 20:48:44 -0300

    server: add pedidos in descobrir route

--- src/routes/pedido.js
+++ src/routes/pedido.js
@@ -12,9 +12,22 @@ router.get('/descobrir', async (req, res) => {
     const conn = await db.connection();
     const [pedidos] = await conn.query(`SELECT * FROM pedido INNER JOIN usuario ON
     cd_usuario_pedido = cd_usuario;`);
+    for(let i = 0; i<pedidos.length; i++){
+        let alPedido = [];
+        const id = pedidos[i].cd_pedido;
+        const [alimentos] = await conn.query(`SELECT nm_alimento FROM alimento
+        WHERE cd_pedido_alimento = ?`, [id]);
+        alimentos.forEach(alimento => {
+            alPedido.push(alimento.nm_alimento);
+        })
+        pedidos[i].comida = alPedido;
+    }
+
     res.render('pedidos/descobrir', {
         pedido: pedidos
     })
+
+    await conn.end();
 })
 
 router.post('/cadpedido', newOrderController.newOrder);
