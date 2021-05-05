f0be2aaae40746284581c3e20635d0db92c188e7
pednucci <pedropetenucci@gmail.com>
2021-05-05 00:59:20 -0300

    server: route pedido added

--- src/routes/pedido.js
+++ src/routes/pedido.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const newOrderController = require('../controllers/newOrderController');
+const { isAuth } = require('../helpers/isAuth');
 
-router.get('/criar', (req, res) => {
+router.get('/criar', isAuth, (req, res) => {
     res.render('pedidos/cadastrar-pedido')
 })
 
+router.post('/cadpedido', newOrderController.newOrder);
+
+router.get('/cadaccept', (req, res) => {
+    req.flash('successMsg', 'Pedido cadastrado com sucesso!')
+    res.redirect('/');
+})
+
+router.get('/cadrecused', (req, res) => {
+    req.flash('errorMsg', 'Ocorreu um erro ao cadastrar seu pedido!')
+    res.redirect('/');
+})
+
 module.exports = router
+
+
