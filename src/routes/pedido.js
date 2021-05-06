07b53de288fc8a0a0ea93071baf67cc827c99626
pednucci <pedropetenucci@gmail.com>
2021-05-05 23:35:09 -0300

    server: ajuda route added

--- src/routes/pedido.js
+++ src/routes/pedido.js
@@ -48,6 +48,62 @@ router.get('/descobrir/pedido/:id', async (req, res) => {
     await conn.end();
 })
 
+router.get('/descobrir/pedido/:id/ajudar', isAuth, async (req, res) => {
+    const conn = await db.connection();
+    const idPed = req.params.id;
+    const [alimento] = await conn.query(`SELECT * FROM alimento INNER JOIN pedido ON
+     cd_pedido_alimento = cd_pedido WHERE cd_pedido_alimento = ?`, [idPed]);
+
+    res.render('pedidos/contribuir', {
+        alimento: alimento
+    })
+    await conn.end();
+})
+
+router.post('/ajudar', async (req, res) => {
+    const conn = await db.connection();
+    const idPedido = req.body.idPedido;
+
+
+    const comentario = req.body.descricao;
+
+    try{
+        const idUser = req.user[0].cd_usuario;
+        console.log(idUser)
+
+        const solicitacao = await conn.query(`INSERT INTO solicitacao(cd_pedido_solicitacao,
+        ds_comentario_solicitacao, cd_usuario_solicitacao) VALUES(?,?,?)`,[idPedido[0], comentario, idUser]);
+
+        const idSoli = solicitacao[0].insertId;
+
+        if(typeof req.body.qtd == 'object'){
+            for(let i = 0; i<req.body.qtd.length; i++) {
+                await conn.query(`INSERT INTO donation
+                (nm_alimento_donation, qt_contribuicao_donation, cd_solicitacao_donation,
+                cd_pedido_donation)
+                VALUES(?,?,?,?)`
+                , [req.body.nmAlimento[i], req.body.qtd[i], idSoli,
+                idPedido[0]])
+            }
+        }
+        if(typeof req.body.qtd == 'string'){
+            await conn.query(`INSERT INTO donation
+                (nm_alimento_donation, qt_contribuicao_donation, cd_solicitacao_donation,
+                cd_pedido_donation)
+                VALUES(?,?,?,?)`
+                , [req.body.nmAlimento, req.body.qtd, idSoli,
+                idPedido[0]])
+        }
+        req.flash('successMsg', 'Solicitação enviada com sucesso, espere a resposta do donatário!');
+        res.redirect('/')
+    }
+    catch(err){
+        console.log(err)
+        req.flash('errorMsg', 'Erro inesperado ao enviar ajuda!');
+        res.redirect('/')
+    }
+})
+
 router.post('/cadpedido', newOrderController.newOrder);
 
 module.exports = router
