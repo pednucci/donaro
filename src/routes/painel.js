71ce23582750ec055516841f232c511e7936c8c1
pednucci <pedropetenucci@gmail.com>
2021-05-06 18:46:27 -0300

    server: painel route added

--- src/routes/painel.js
+++ src/routes/painel.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const db = require('../database/database');
 
 router.get('/pedidos', async (req, res) => {
-    res.render('painel/meus-pedidos')
+    const conn = await db.connection();
+    const idUser = req.user[0].cd_usuario;
+    const [pedidos] = await conn.query(`SELECT * FROM pedido WHERE cd_usuario_pedido = ?`,[idUser]);
+    res.render('painel/meus-pedidos', {
+        pedido: pedidos
+    })
 })
 
 module.exports = router;
