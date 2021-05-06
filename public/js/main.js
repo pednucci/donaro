819461f02465a62de7b3dd0dcae1185ed8d6b620
Yago Mouro <yagormouro@gmail.com>
2021-05-06 13:50:04 -0300

    web: update form style

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,46 @@
+var navbar = document.querySelector('.nav-bar');
 var navProfile = document.querySelector('.nav-profile');
 var toggleMenu = document.querySelector('.toggle-menu');
 var display = toggleMenu.style.display;
-// console.log(display)
+
+
 
 navProfile.addEventListener('click', () => {
-  if (toggleMenu.style.display == "none") {
+  if (display == "block") {
     toggleMenu.style.display = "none";
   } else {
     toggleMenu.style.display = "block";
   }
+  display = toggleMenu.style.display;
+
 })
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 0) {
+//     navbar.style.position = "fixed"
+//     // navbar.style.display = "none"
+//   } else {
+//     navbar.style.position = "initial"
+//     // navbar.style.display = "block"
+//   }
+
+// })
+
+// // $(window).scroll(function () {
+// //     if ($(window).scrollTop() > 50) {
+// //         $(".header").addClass("scroll");
+// //     } else {
+// //         $(".header").removeClass("scroll");
+// //     }
+// // });
+
+// // // Header hidden
+// // var prev = 0;
+// // var $window = $(window);
+// // var nav = $('.header');
+
+// // $window.on('scroll', function () {
+// //     var scrollTop = $window.scrollTop();
+// //     nav.toggleClass('hidden', scrollTop > prev);
+// //     prev = scrollTop;
+// // });
