b62eb75bd6f6463b0143898b19a3495524c282d0
pednucci <pedropetenucci@gmail.com>
2021-05-20 17:34:47 -0300

    fixed

--- src/controllers/newOrderController.js
+++ src/controllers/newOrderController.js
@@ -7,7 +7,7 @@ exports.newOrder = async (req, res) => {
     var erros = [];
     const conn = await db.connection();
     const titulo = req.body.titulo;
-    const dtPed = new Date(req.body.dtPedido);
+    const dtPed = req.body.dtPedido;
     const estado = req.body.estado;
     const cidade = req.body.cidade;
     const desc = req.body.descPed;
@@ -15,10 +15,10 @@ exports.newOrder = async (req, res) => {
     let uploadPath;
 
     try {
-        if (isPast(new Date(dtPed))) {
-            erros.push({ text: 'Selecione uma data válida!' })
+        if(isPast(new Date(dtPed))){
+            erros.push({text: 'Selecione uma data válida!'})
         }
-        if (erros.length > 0) {
+        if(erros.length > 0){
             const conn = await db.connection();
             const [produtos] = await conn.query('SELECT * FROM produto')
             res.render('pedidos/cadastrar-pedido', {
@@ -26,71 +26,71 @@ exports.newOrder = async (req, res) => {
                 produtos
             })
             await conn.end();
-        } else {
+        }else{
             const idUser = req.user[0].cd_usuario;
             const pedido = await conn.query(`INSERT INTO pedido(cd_usuario_pedido, nm_titulo_pedido,
                 dt_encerramento_pedido, nm_cidade_pedido,
                 sg_estado_pedido, ds_acao_pedido) VALUES(?,?,?,?,?,?)
                 `, [idUser, titulo, dtPed, cidade, estado, desc]);
-
+    
             const idPedido = pedido[0].insertId;
-
-
-            if (req.files) {
+            
+    
+            if(req.files){
                 fileUpload = req.files.picPed;
-                uploadPath = path.join(__dirname, '..', '..', '/public/assets/pedidos/' + fileUpload.name);
+                uploadPath = path.join(__dirname, '..', '..', '/public/assets/pedidos/' + fileUpload.name)  ;
                 fileUpload.mv(uploadPath, async (err) => {
-                    if (err) {
+                    if(err){
                         req.flash("errorMsg", "Houve um erro ao cadastrar o pedido!")
                         console.log(err)
                         res.redirect('/')
                     }
-                    else {
+                    else{
                         await conn.query(`UPDATE pedido SET cd_imagem_pedido = ? WHERE
                         cd_pedido = ?`, [fileUpload.name, idPedido])
                     }
                 })
             }
-
-            if (typeof req.body.alimentoInput == 'object') {
-                for (let i = 0; i < req.body.alimentoInput.length; i++) {
-
+            
+            if(typeof req.body.alimentoInput == 'object'){
+                for(let i = 0; i<req.body.alimentoInput.length; i++) {
+    
                     let medida = await conn.query(`SELECT sg_medida_produto FROM produto WHERE nm_produto =
-                    ? LIMIT 1`, [req.body.alimentoInput[i]])
-
+                    ? LIMIT 1`,[req.body.alimentoInput[i]])
+    
                     await conn.query(`INSERT INTO alimento
                     (nm_alimento, cd_pedido_alimento, nm_medida_alimento, qt_alimento)
                     VALUES(?,?,?,?)`
-                        , [req.body.alimentoInput[i], idPedido, medida[0][0].sg_medida_produto,
-                        req.body.quantidade[i]])
+                    , [req.body.alimentoInput[i], idPedido, medida[0][0].sg_medida_produto,
+                     req.body.quantidade[i]])
                 }
             }
-            if (typeof req.body.alimentoInput == 'string') {
-
+            if(typeof req.body.alimentoInput == 'string'){
+                
                 let medida = await conn.query(`SELECT sg_medida_produto FROM produto WHERE nm_produto =
-                ? LIMIT 1`, [req.body.alimentoInput])
-
+                ? LIMIT 1`,[req.body.alimentoInput])
+    
                 await conn.query(`INSERT INTO alimento
                     (nm_alimento, cd_pedido_alimento, nm_medida_alimento, qt_alimento)
                     VALUES(?,?,?,?)`
                     , [req.body.alimentoInput, idPedido, medida[0][0].sg_medida_produto,
-                    req.body.quantidade])
+                     req.body.quantidade])
             }
-
+    
             await conn.query(`
             UPDATE pedido SET qt_total_pedido = 
             (select sum(qt_alimento) from alimento where cd_pedido_alimento = ?) WHERE cd_pedido = ?
             `, [idPedido, idPedido])
-
+    
             req.flash('successMsg', 'Pedido cadastrado com sucesso!')
             res.redirect('/painel/pedidos')
         }
     }
-    catch (err) {
+    catch(err) {
         console.log(err)
         req.flash('errorMsg', 'Erro ao cadastrar pedido!')
         res.redirect('/')
     }
-
+    
     await conn.end();
 }
