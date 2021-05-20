eb122c0f2bd15c2c9d65fa849b459e94fe640444
pednucci <pedropetenucci@gmail.com>
2021-05-20 18:25:24 -0300

    server: scope update

--- src/routes/notificacao.js
+++ src/routes/notificacao.js
@@ -8,8 +8,8 @@ router.get('/', async (req, res) => {
 
     const [notificacao] = await conn.query(`SELECT * FROM solicitacao
     INNER JOIN pedido ON cd_pedido_solicitacao = cd_pedido INNER JOIN usuario ON
-     cd_usuario_solicitacao = cd_usuario WHERE cd_avaliacao_admin_pedido = 1 AND 
-     cd_situacao_solicitacao = 'A CONFIRMAR' and cd_usuario_pedido = ?`, [idUser])
+     cd_usuario_solicitacao = cd_usuario WHERE 
+     cd_situacao_solicitacao = 'A CONFIRMAR' AND cd_usuario_pedido = ?`, [idUser])
 
     res.render('notificacao/notificacao-ajuda', {
         notificacao: notificacao
@@ -23,11 +23,10 @@ router.get('/one/:id', async (req, res) => {
     const idUser = req.user[0].cd_usuario;
     const idSoli = req.params.id;
 
-    const [notificacao] = await conn.query(`SELECT * FROM solicitacao
-     INNER JOIN pedido ON cd_usuario_pedido = ? INNER JOIN usuario ON
-      cd_usuario_solicitacao = cd_usuario WHERE cd_avaliacao_admin_pedido = 1 AND cd_solicitacao = ?
-      AND cd_situacao_solicitacao = 'A CONFIRMAR'`,
-    [idUser, idSoli]);
+    const [notificacao] = await conn.query(`SELECT * FROM solicitacao  INNER JOIN usuario ON
+    cd_usuario_solicitacao = cd_usuario WHERE cd_solicitacao = ?
+    AND cd_situacao_solicitacao = 'A CONFIRMAR';`,
+    [idSoli]);
 
     const [alimento] = await conn.query(`SELECT * FROM donation INNER JOIN alimento ON 
     nm_alimento_donation = nm_alimento WHERE cd_solicitacao_donation = ? AND cd_pedido_alimento = 
@@ -46,36 +45,10 @@ router.get('/doacoes', async (req, res) => {
     const idUser = req.user[0].cd_usuario;
     const [notificacao] = await conn.query(`SELECT * FROM solicitacao INNER JOIN
     pedido ON cd_pedido_solicitacao = cd_pedido INNER JOIN usuario ON cd_usuario_pedido = cd_usuario
-    WHERE cd_aceito_solicitacao >= 0 AND cd_usuario_solicitacao = ?`, [idUser])
+    WHERE cd_usuario_solicitacao = ?`, [idUser])
     res.render('notificacao/notificacao-doacoes', {
         notificacao: notificacao
     })
-
-    await conn.end();
-})
-
-router.post('/accept', async (req, res) => {
-    const conn = await db.connection();
-    const idSoli = req.body.idSoli;
-
-    await conn.query(`UPDATE solicitacao SET cd_aceito_solicitacao = 1, cd_situacao_solicitacao =
-    'PENDENTE' WHERE cd_solicitacao = ?`, [idSoli]);
-
-    req.flash('successMsg', 'Ajuda aceita!');
-    res.redirect('/');
-    
-    await conn.end();
-})
-
-router.post('/recused', async (req, res) => {
-    const conn = await db.connection();
-    const idSoli = req.body.idSoli;
-
-    await conn.query(`UPDATE solicitacao SET cd_aceito_solicitacao = 0, cd_situacao_solicitacao =
-    'RECUSADA' WHERE cd_solicitacao = ?`, [idSoli]);
-
-    req.flash('successMsg', 'Ajuda recusada');
-    res.redirect('/');
 
     await conn.end();
 })
