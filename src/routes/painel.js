5be45557aeb6aced174007e9697fc8b93431f961
pednucci <pedropetenucci@gmail.com>
2021-05-21 21:41:32 -0300

    server: individual order on the panel

--- src/routes/painel.js
+++ src/routes/painel.js
@@ -12,4 +12,26 @@ router.get('/pedidos', async (req, res) => {
     await conn.end();
 })
 
+router.get('/pedidos/:id', async (req, res) => {
+    const conn = await db.connection();
+    const idUser = req.user[0].cd_usuario;
+    const idPedido = req.params.id;
+
+    const [pedido] = await conn.query(`SELECT * FROM pedido WHERE cd_usuario_pedido = ? AND
+    cd_pedido = ?`, [idUser, idPedido])
+
+    const [notificacao] = await conn.query(`SELECT * FROM solicitacao
+    INNER JOIN pedido ON cd_pedido_solicitacao = cd_pedido INNER JOIN usuario ON
+     cd_usuario_solicitacao = cd_usuario WHERE 
+     cd_situacao_solicitacao = 'A CONFIRMAR' AND cd_usuario_pedido = ? AND
+     cd_pedido_solicitacao = ?`, [idUser, idPedido])
+       
+    res.render('painel/meus-pedidos-ajudas', {
+        notificacao,
+        pedido
+    })
+
+    await conn.end();
+})
+
 module.exports = router;
