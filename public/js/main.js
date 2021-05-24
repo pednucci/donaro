a5398fe8eeb9527f819cd057fcb97a9e4ef28150
Yago Mouro <yagormouro@gmail.com>
2021-05-24 20:47:17 -0300

    public: pedido-alimento fixed

--- public/js/main.js
+++ public/js/main.js
@@ -3,34 +3,14 @@ var currentScrollY = window.scrollY;
 var lastScrollY = 0;
 
 
-window.addEventListener("scroll", () => {
-  var currentScrollY = window.scrollY;
+// window.addEventListener("scroll", () => {
+//   var currentScrollY = window.scrollY;
 
-  if (currentScrollY > lastScrollY) {
-    navbar.classList = "nav-bar hidden"
-  } else {
-    navbar.classList = "nav-bar"
-  }
-  lastScrollY = currentScrollY;
+//   if (currentScrollY > lastScrollY) {
+//     navbar.classList = "nav-bar hidden"
+//   } else {
+//     navbar.classList = "nav-bar"
+//   }
+//   lastScrollY = currentScrollY;
 
-})
 // })
-
-// $(window).scroll(function () {
-//     if ($(window).scrollTop() > 50) {
-//         $(".header").addClass("scroll");
-//     } else {
-//         $(".header").removeClass("scroll");
-//     }
-// });
-
-// // Header hidden
-// var prev = 0;
-// var $window = $(window);
-// var nav = $('.header');
-
-// $window.on('scroll', function () {
-//     var scrollTop = $window.scrollTop();
-//     nav.toggleClass('hidden', scrollTop > prev);
-//     prev = scrollTop;
-//
