81262fd8c1913f3ad385c376fe9e16e748f70039
pednucci <pedropetenucci@gmail.com>
2021-05-26 16:53:33 -0300

    server: websocket config

---
+++ src/Websocket.js
@@ -0,0 +1,28 @@
+const { io } = require('./server');
+const { format } = require('date-fns');
+const db = require('./database/database');
+
+io.on('connection', async socket => {
+    let link = socket.handshake.headers.referer;
+    var room = link.split('/')[4];
+    socket.join(room);
+    const userConnect = socket.handshake.session.passport.user;
+    socket.emit('connected', userConnect)
+    socket.on('sendMessage', async msg => {
+        const conn = await db.connection();
+        const idUser = socket.handshake.session.passport.user;
+        const time = format(Date.now(), 'hh:mm');
+        console.log(idUser)
+        let messageObject = {
+            msg: msg.message,
+            time,
+            idUser,
+            read: msg.read,
+            room
+        }
+        io.to(room).emit('sendMessage', messageObject);
+        await conn.query(`INSERT INTO mensagem(ds_conteudo_mensagem, cd_chat_mensagem,
+        cd_usuario_mensagem) VALUES(?,?,?)`,[msg.message, room, idUser])
+        await conn.end();
+    })
+})
