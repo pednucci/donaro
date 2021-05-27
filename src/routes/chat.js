1a99ce16533a13a5a793f640762c0a578477152c
pednucci <pedropetenucci@gmail.com>
2021-05-27 14:56:11 -0300

    server: query chat fix

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -31,9 +31,14 @@ router.get('/:soli', async (req, res) => {
         cd_pedido_solicitacao = cd_pedido
         WHERE cd_solicitacao = ?`, idSoli);
         if(soli[0].cd_usuario_solicitacao == idUser){
+            const [pedido] = await conn.query(`SELECT cd_pedido FROM pedido WHERE 
+            cd_pedido = (SELECT cd_pedido_solicitacao FROM solicitacao
+            WHERE cd_solicitacao = ?)`,[idSoli]);
+
             const [userPedido] = await conn.query(`SELECT * FROM usuario
             INNER JOIN pedido ON cd_usuario = cd_usuario_pedido WHERE cd_usuario = (SELECT
-            cd_userPedido_chat FROM chat WHERE cd_solicitacao_chat = ?)`,[idSoli]);
+            cd_userPedido_chat FROM chat WHERE cd_solicitacao_chat = ?) AND
+            cd_pedido = ?`,[idSoli, pedido[0].cd_pedido]);
             const [mensagem] = await conn.query(`SELECT * FROM mensagem WHERE cd_chat_mensagem
             = ?`, [idSoli]);
             const [chatCampanhas] = await conn.query(`SELECT * FROM chat 
