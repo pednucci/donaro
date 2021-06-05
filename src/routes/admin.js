ff916e6844f287644b4d4a19ef8735efc4fba0f8
pednucci <pedropetenucci@gmail.com>
2021-06-04 21:53:27 -0300

    server: new admin routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,59 +2,28 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database/database');
 
-router.get('/pedidos', async (req, res) => {
-    const conn = await db.connection();
-    const [pedidos] = await conn.query(`SELECT * FROM pedido WHERE cd_situacao_pedido = 'PENDENTE'`);
-    res.render('adm/requisicoes', {
-        pedidos: pedidos
-    })
-    await conn.end();
+router.get('/denuncias/campanhas', (req, res) => {
+    res.render('adm/denuncias-pedidos')
 })
 
-router.get('/pedidos/:id', async (req, res) => {
-    const conn = await db.connection();
-    const id = req.params.id;
-
-    const [acceptOrNot] = await conn.query(`SELECT cd_avaliacao_admin_pedido FROM pedido
-    WHERE cd_pedido = ?`, [id]);
-
-    if(acceptOrNot[0].cd_avaliacao_admin_pedido == 1 || 0){
-        req.flash("errorMsg", "O pedido já foi avaliado!");
-        res.redirect('/admin/pedidos');
-        return
-    }
-
-    const [pedido] = await conn.query('SELECT * FROM pedido WHERE cd_pedido = ?',[id]);
-    const [alimento] = await conn.query('SELECT * FROM alimento WHERE cd_pedido_alimento = ?', [id]);
-    const [usuario] = await conn.query(`SELECT * FROM usuario WHERE cd_usuario =
-    (SELECT cd_usuario_pedido FROM pedido WHERE cd_pedido = ?)
-    `,[id])
-    res.render('adm/requisicao', {
-        pedido: pedido,
-        alimento: alimento,
-        usuario: usuario
-    })
-    await conn.end();
+router.get('/denuncias/campanhas/:id', (req, res) => {
+    res.render('adm/denuncias-pedido')
 })
 
-router.post('/accept', async (req, res) => {
-    const idPed = req.body.idPedido;
-    const conn = await db.connection();
-    await conn.query(`UPDATE pedido SET cd_avaliacao_admin_pedido = 1,
-    cd_situacao_pedido = 'ACEITO' WHERE cd_pedido = ?`,[idPed]);
-    req.flash('successMsg', 'Pedido aprovado!');
-    res.redirect('/admin/pedidos');
-    await conn.end();
+router.get('/denuncias/naoentregues', (req, res) => {
+    res.render('adm/pedidos-nao-entregues')
 })
 
-router.post('/recused', async (req, res) => {
-    const idPed = req.body.idPedido;
-    const conn = await db.connection();
-    await conn.query(`UPDATE pedido SET cd_avaliacao_admin_pedido = 0,
-    cd_situacao_pedido = 'RECUSADO' WHERE cd_pedido = ?`, [idPed]);
-    req.flash('successMsg', 'Pedido recusado');
-    res.redirect('/admin/pedidos');
-    await conn.end();
+router.get('/denuncias/naoentregues/:id', (req, res) => {
+    res.render('adm/pedido-nao-entregue')
+})
+
+router.get('/denuncias/usuarios', (req, res) => {
+    res.render('adm/denuncias-usuarios')
+})
+
+router.get('/denuncias/usuarios/:id', (req, res) => {
+    res.render('adm/denuncias-usuario')
 })
 
 module.exports = router;
