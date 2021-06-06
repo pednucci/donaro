088bf61e90d468df39eb5ea001136eadb9bb72d0
pednucci <pedropetenucci@gmail.com>
2021-06-06 19:21:36 -0300

    server: more denuncias routes added

--- src/routes/denuncia.js
+++ src/routes/denuncia.js
@@ -5,4 +5,12 @@ router.get('/descobrir/pedido/:id/denuncia', (req, res) => {
     res.render('denuncias/denuncia-pedido')
 })
 
+router.get('/painel/pedido/:pedido/:soli/naoentregue', (req, res) => {
+    res.render('denuncias/nao-entregue')
+})
+
+router.get('/usuario/:id/denuncia', (req, res) => {
+    res.render('denuncias/denuncia-usuario')
+})
+
 module.exports = router;
