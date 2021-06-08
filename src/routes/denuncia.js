7c16bbc0909896d82b6dd96f6311d04c0456a558
pednucci <pedropetenucci@gmail.com>
2021-06-08 01:48:19 -0300

    server: more validations

--- src/routes/denuncia.js
+++ src/routes/denuncia.js
@@ -3,29 +3,43 @@ const router = express.Router();
 const db = require('../database/database');
 const { isAuth } = require('../helpers/isAuth');
 
-router.get('/descobrir/pedido/:id/denuncia', isAuth, (req, res) => {
-    res.render('denuncias/denuncia-pedido')
+router.get('/descobrir/pedido/:id/denuncia', isAuth, async (req, res) => {
+    const conn = await db.connection();
+    const [ifExist] = await conn.query(`SELECT count(*) AS count FROM solicitacao WHERE
+    cd_solicitacao = ?`,[req.params.id])
+    if(ifExist[0].count == 1){
+        res.render('denuncias/denuncia-pedido')
+    }
+    else{
+        res.redirect('/')
+    }
 })
 
 router.post('/descobrir/pedido/:id/denuncia', async (req, res) => {
     const conn = await db.connection();
-    const motivo = req.body.motivo;
-    const descricao = req.body.descricao;
-    const idUser = req.user[0].cd_usuario;
-    const idPedido = req.params.id;
-    try{
-        await conn.query(`INSERT INTO denuncia(cd_usuario_denuncia, nm_motivo_denuncia,
-        ds_descricao_denuncia, cd_pedido_denunciado) VALUES(?,?,?,?)`,
-        [idUser, motivo, descricao, idPedido]);
-        req.flash('successMsg', 'Denúncia enviada com sucesso');
-        res.redirect('/descobrir')
+    const [ifExist] = await conn.query(`SELECT count(*) AS count FROM solicitacao WHERE
+    cd_solicitacao = ?`,[req.params.id])
+    if(ifExist[0].count == 1){
+        const motivo = req.body.motivo;
+        const descricao = req.body.descricao;
+        const idUser = req.user[0].cd_usuario;
+        const idPedido = req.params.id;
+        try{
+            await conn.query(`INSERT INTO denuncia(cd_usuario_denuncia, nm_motivo_denuncia,
+            ds_descricao_denuncia, cd_pedido_denunciado) VALUES(?,?,?,?)`,
+            [idUser, motivo, descricao, idPedido]);
+            req.flash('successMsg', 'Denúncia enviada com sucesso');
+            res.redirect('/descobrir')
+        }
+        catch(err){
+            console.log(err);
+            req.flash('errorMsg', 'Erro inesperado');
+            res.redirect('/descobrir')
+        }
     }
-    catch(err){
-        console.log(err);
-        req.flash('errorMsg', 'Erro inesperado');
-        res.redirect('/descobrir')
+    else{
+        res.redirect('/')
     }
-    
 })
 
 router.get('/painel/pedidos/:pedido/:soli/naoentregue', isAuth, async (req, res) => {
@@ -71,8 +85,9 @@ router.post('/painel/pedidos/:pedido/:soli/naoentregue', isAuth , async (req, re
             'NÃO ENTREGUE' WHERE
             cd_solicitacao = ?`, [req.params.soli])
             await conn.query(`INSERT INTO denuncia(cd_usuario_denuncia, nm_motivo_denuncia,
-            ds_descricao_denuncia, cd_usuario_denunciado) VALUES(?,'Produto não entregue', ?, ?)`,
-            [idUser, descricao, usuarioDenunciado[0].user]);
+            ds_descricao_denuncia, cd_usuario_denunciado,
+            cd_solicitacao_denunciada) VALUES(?,'Produto não entregue', ?, ?, ?)`,
+            [idUser, descricao, usuarioDenunciado[0].user, req.params.soli]);
             req.flash('successMsg', 'Resposta enviada');
             res.redirect(`/painel/pedidos/${req.params.pedido}`)
         }
