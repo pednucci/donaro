432f81dd57c28544f6f84d6ccf93068f19343ea5
pednucci <pedropetenucci@gmail.com>
2021-06-19 21:02:58 -0300

    server: update on notification routes

--- src/routes/notificacao.js
+++ src/routes/notificacao.js
@@ -6,47 +6,15 @@ router.get('/', async (req, res) => {
     const conn = await db.connection();
     const idUser = req.user[0].cd_usuario;
 
-    const [notificacao] = await conn.query(`SELECT * FROM solicitacao
-    INNER JOIN pedido ON cd_pedido_solicitacao = cd_pedido INNER JOIN usuario ON
-     cd_usuario_solicitacao = cd_usuario WHERE 
-    cd_usuario_pedido = ?`, [idUser])
+    const [notificacao] = await conn.query(`SELECT * FROM notificacao
+    INNER JOIN solicitacao ON cd_solicitacao_notificacao = cd_solicitacao 
+    INNER JOIN pedido ON cd_pedido_solicitacao = cd_pedido INNER JOIN
+    usuario ON cd_usuario_solicitacao = cd_usuario INNER JOIN
+    chat ON cd_usuario = cd_userSoli_chat WHERE 
+    cd_usuario_notificacao = ?`, [idUser])
 
-    res.render('notificacao/notificacao-ajuda', {
-        notificacao: notificacao
-    })
-
-    await conn.end();
-})
-
-router.get('/one/:id', async (req, res) => {
-    const conn = await db.connection();
-    const idUser = req.user[0].cd_usuario;
-    const idSoli = req.params.id;
-
-    const [notificacao] = await conn.query(`SELECT * FROM solicitacao  INNER JOIN usuario ON
-    cd_usuario_solicitacao = cd_usuario WHERE cd_solicitacao = ?;`,
-    [idSoli]);
-
-    const [alimento] = await conn.query(`SELECT * FROM donation INNER JOIN alimento ON 
-    nm_alimento_donation = nm_alimento WHERE cd_solicitacao_donation = ? AND cd_pedido_alimento = 
-    (SELECT cd_pedido_solicitacao FROM solicitacao WHERE cd_solicitacao = ?);;`,[idSoli, idSoli])
-
-    res.render('notificacao/notificacao-aceitar', {
-        notificacao: notificacao,
-        alimento: alimento
-    })
-
-    await conn.end();
-})
-
-router.get('/doacoes', async (req, res) => {
-    const conn = await db.connection();
-    const idUser = req.user[0].cd_usuario;
-    const [notificacao] = await conn.query(`SELECT * FROM solicitacao INNER JOIN
-    pedido ON cd_pedido_solicitacao = cd_pedido INNER JOIN usuario ON cd_usuario_pedido = cd_usuario
-    WHERE cd_usuario_solicitacao = ?`, [idUser])
-    res.render('notificacao/notificacao-doacoes', {
-        notificacao: notificacao
+    res.render('notificacao/notificacao', {
+        notificacao
     })
 
     await conn.end();
